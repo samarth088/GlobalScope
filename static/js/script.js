5ebea775da9f08bc250024ec2b12e80a70ce9715
samarth088 <skshah987654@gmail.com>
2025-06-08 08:24:16 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Add a Refresh Button
     const container = document.querySelector('.container');
     const refreshBtn = document.createElement('button');
     refreshBtn.className = 'btn btn-secondary mb-4';
     refreshBtn.innerText = 'Refresh News';
     refreshBtn.onclick = function() {
-        location.reload();
+        fetch('/fetch-articles')
+            .then(response => response.text())
+            .then(() => location.reload());
     };
     container.insertBefore(refreshBtn, container.firstChild);
+
+    // Auto-refresh every 5 minutes
+    setInterval(() => {
+        fetch('/fetch-articles')
+            .then(response => response.text())
+            .then(() => location.reload());
+    }, 300000); // 5 minutes
 });
